7534afced746761f98c092e25d76cde7a2e6a1ca
maruthan4r <mtcreatives@hotmail.com>
2023-11-15 21:55:00 +0100

    apply design "ahorn"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import './styles/globals.css'
 import './styles/home.css'
-import './styles/section-window.css'
-import './styles/intro-grid.css'
 
 export const metadata = {
   title: 'MARUTHAN.COM',
--- src/app/page.js
+++ src/app/page.js
@@ -5,66 +5,16 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 
 export default function Home() {
-  const openSection = (id) => {
-    const item = document.getElementById("section-window");
-    item.style.backgroundColor = "var(--section-" + id + "-color)";
-
-    item.classList.add(["section-window-active"]);
-  };
-
   return (
     <main>
-      <div className="intro">
-        <div className="intro-grid">
-          <div className="grid-item-1">
-            <button
-              className="grid-item-1-bubble"
-              onClick={() => {
-                openSection(0);
-              }}
-            >
-              <div className="grid-item-text">about</div>
-            </button>
-          </div>
-          <div className="grid-item-2">
-            <button
-              className="grid-item-2-bubble"
-              onClick={() => {
-                openSection(1);
-              }}
-            >
-              <div className="grid-item-text">gallery</div>
-            </button>
-          </div>
-          <div className="grid-item-3">
-            <button
-              className="grid-item-3-bubble"
-              onClick={() => {
-                openSection(2);
-              }}
-            >
-              <div className="grid-item-text">services</div>
-            </button>
-          </div>
-          <div
-            className="grid-item-4"
-            onClick={() => {
-              openSection(3);
-            }}
-          >
-            <button className="grid-item-4-bubble">
-              <div className="grid-item-text">contact</div>
-            </button>
-          </div>
-          <div className="grid-item-5">
-            <button className="grid-item-5-bubble">
-              <div className="title">MARUTHAN THANABALASINGAM</div>
-            </button>
+      <div className="section-room">
+        <div className="navbar"></div>
+        <div className="section-room-text">
+          <div className="section-room-title">Maruthan Thanabalasingam</div>
+          <div className="section-room-description">
+            I love to design & build software.
           </div>
         </div>
-      </div>
-      <div className="section-window-container" id="section-window-container">
-        <div className="section-window" id="section-window"></div>
       </div>
     </main>
   );
