1e70c9d7146b11b6911bc02b5f4b8baa5f120c05
maruthan4r <mtcreatives@hotmail.com>
2023-12-06 17:09:08 +0100

    add social cards

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,17 +5,32 @@ import "./styles/room.css";
 import "./styles/projects.css";
 import "./styles/blog.css";
 import "./styles/socials.css";
-
-export const metadata = {
-  title: "maruthan.com",
-  description: "Develop, design, create.",
-};
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <Head>
+        <title>Maruthan.com</title>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@THEM2DT" />
+        <meta name="twitter:title" content="Maruthan.com" />
+        <meta name="twitter:description" content="I create." />
+        <meta
+          name="twitter:image"
+          content="https://bafkreiga27mlvbr4lc6luxnqhxyqw7ksfh4hc6s2hshbnjj5zgisnfahn4.ipfs.nftstorage.link/"
+        />
+
+        <meta property="og:title" content="Maruthan.com" />
+        <meta property="og:description" content="I create." />
+        <meta
+          property="og:image"
+          content="https://bafkreiga27mlvbr4lc6luxnqhxyqw7ksfh4hc6s2hshbnjj5zgisnfahn4.ipfs.nftstorage.link/"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+      </Head>
       <body>{children}</body>
     </html>
   );
