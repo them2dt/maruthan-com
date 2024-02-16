f4ee8035a5c8c11035ccaf05638c09373ae484cd
maruthan4r <mtcreatives@hotmail.com>
2024-02-16 08:51:05 +0100

    flexify

--- components/hero/index.js
+++ components/hero/index.js
@@ -8,19 +8,18 @@ export default function Index({ toggleMode }) {
 
   return (
     <motion.div className="hero fps" id="hero">
-      <motion.div className="hero-title font-title">
-        Maruthan Thanabalasingam
-      </motion.div>
-      <motion.div className="hero-description font-text">
-        WEB - MOBILE - BLOCKCHAIN
-      </motion.div>
+      <div className="hero-text">
+        <motion.div className="hero-title font-title">
+          Maruthan Thanabalasingam
+        </motion.div>
+        <motion.div className="hero-description font-text">
+          WEB - MOBILE - BLOCKCHAIN
+        </motion.div>
+      </div>
 
       <motion.div className="hero-graphic-container">
         <motion.div className="hero-graphic">
-          <motion.div
-            className="drill"
-            onClick={toggleMode}
-          >
+          <motion.div className="drill" onClick={toggleMode}>
             <motion.div></motion.div>
             <motion.div></motion.div>
 
